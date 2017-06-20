91c79fd83118e4661947b47cdee8294074d882d9
crueda <crueda@gmail.com>
2017-06-20 13:29:31 +0200

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { LanguageService } from './language-service.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DirectivesExampleComponent } from './directives-example/directives-example.component';
 import { DecoratorPipe } from './pipes/decorator.pipe';
 import { DataBindingExampleComponent } from './data-binding-example/data-binding-example.component';
 import { DataBindingChildExampleComponent } from './data-binding-child-example/data-binding-child-example.component';
+
+const ROUTES: Routes = [
+ {path: '', redirectTo: 'directives', pathMatch: 'full'},
+ {path: 'directives', component: DirectivesExampleComponent},
+ {path: 'databinding', component: DataBindingExampleComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +24,17 @@ import { DataBindingChildExampleComponent } from './data-binding-child-example/d
     DataBindingChildExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
-  bootstrap: [DataBindingExampleComponent]
+  providers: [
+    LanguageService,
+    { provide: LOCALE_ID, 
+      deps: [LanguageService],
+      useFactory: (LanguageService: LanguageService) => LanguageService.getLanguage()
+    }
+  ],
+  //bootstrap: [DataBindingExampleComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/directives-example/directives-example.component.ts
+++ src/app/directives-example/directives-example.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ColorsService } from './../colors.service';
+//import { LanguageServiceService } from './../language-service.service';
+import { Component, OnInit, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-directives-example',
   templateUrl: './directives-example.component.html',
-  styleUrls: ['./directives-example.component.css']
+  styleUrls: ['./directives-example.component.css'],
+  providers: [ColorsService]
 })
 export class DirectivesExampleComponent implements OnInit {
 
   show: boolean;
   colors: string[];
-
-  constructor() { }
+  language: string;
+  // tslint:disable-next-line:no-trailing-whitespace
+  
+  constructor(@Inject(ColorsService) private colorsService: ColorsService/*, @Inject(LanguageServiceService) private languageService: LanguageServiceService */) { }
 
   ngOnInit() {
     this.show = false;
-    this.colors = ['red','blue','orange','purple']
+    //this.colors = ['red','blue','orange','purple']
+    this.colors = this.colorsService.getColors();
+//    this.language = this.languageServiceService.getLanguage();
   }
 
 }
