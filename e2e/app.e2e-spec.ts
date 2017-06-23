4552e2491657ee7668d3bdd65b55a968f5c60b6f
crueda <crueda@gmail.com>
2017-06-23 12:30:30 +0200

    dia 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser, element, by } from 'protractor';
 import { CursoAngularPage } from './app.po';
 
 describe('curso-angular App', () => {
@@ -11,4 +12,22 @@ describe('curso-angular App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
+
+  it('Lista de usuarios', () => {
+    browser.get('http://localhost:4200/login')
+    element(by.id('username')).sendKeys('admin')
+    element(by.id('password')).sendKeys('admin')
+    element(by.id('login')).click()
+
+    browser.get('http://localhost:4200/users')
+
+    element(by.id('leer')).click()
+
+    element.all(by .css('li')).then(function(items){
+console.log(items);
+      expect(items.length).toBe(13)
+    })
+
+  });
+
 });
